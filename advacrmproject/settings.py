dcf8d52f8fd0905913157fae167daf1edf33ac5f
nazrul <bdm@advawebs.com>
2024-01-27 08:35:23 +0530

    update docs

--- advacrmproject/settings.py
+++ advacrmproject/settings.py
@@ -12,7 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
+from decouple import AutoConfig
+
+# Create an AutoConfig instance
+config = AutoConfig()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
